0d541cd4c53bd097c2cb13249c3d9f2ed94b5d04
Taras Nek <taras.nek@datarobot.com>
2026-03-18 13:35:11 +0200

    Update license

--- test.py
+++ test.py
@@ -1,3 +1,17 @@
+#!/usr/bin/env python3
+#
+# Copyright 2026 DataRobot, Inc. and its affiliates.
+#
+# All rights reserved.
+#
+# DataRobot, Inc. Confidential.
+#
+# This is unpublished proprietary source code of DataRobot, Inc.
+# and its affiliates.
+#
+# The copyright notice above does not evidence any actual or intended
+# publication of such source code.
+
 import time
 
 print("Hello, World!")
