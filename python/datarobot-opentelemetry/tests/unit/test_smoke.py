d70cb0e67623923d654bdc80d0054f2289996156
Taras Nek <taras.nek@datarobot.com>
2026-03-19 17:56:25 +0200

    sbrush up makefile

--- python/datarobot-opentelemetry/tests/unit/test_smoke.py
+++ python/datarobot-opentelemetry/tests/unit/test_smoke.py
@@ -10,18 +10,6 @@
 # The copyright notice above does not evidence any actual or intended
 # publication of such source code.
 
-# Copyright 2024 DataRobot, Inc. and its affiliates.
-#
-# All rights reserved.
-#
-# DataRobot, Inc. Confidential.
-#
-# This is unpublished proprietary source code of DataRobot, Inc.
-# and its affiliates.
-#
-# The copyright notice above does not evidence any actual or intended
-# publication of such source code.
-
 
 def test_unit_smoke() -> None:
     assert True
